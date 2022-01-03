a704c32d0b994e474537aa0f1c0f6b1ba07880cb
luissky <luizguilhermeabrantes@gmail.com>
2022-01-03 18:55:49 -0300

    change routes directory | refactory

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
-const routes = require('./config/router.js')
+const routes = require('./routes')
 
 const PORT = 3000
 
