16c140859f67616f238608fadb5a1cd8cb3a9fbb
luissky <luizguilhermeabrantes@gmail.com>
2022-03-14 13:18:25 -0300

    change start server message

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
 const app = require('./config/app')
 const env = require('../config/env')
 
-app.listen(env.PORT, () => console.log(`server runnning at ${env.PORT}`))
+app.listen(env.PORT, () => console.log(`server runnning at http://localhost:${env.PORT}`))
