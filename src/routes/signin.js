1dfa815f487ec8e0eac9a3bc3d54b991e62f43ff
luissky <luizguilhermeabrantes@gmail.com>
2022-03-22 12:51:29 -0300

    refactor: remove duplicated httpResponse objects and create HttpResponse class

--- src/routes/signin.js
+++ src/routes/signin.js
@@ -1,5 +1,6 @@
 
 const { MissingParamError } = require('../helpers/errors')
+const HttpResponse = require('./helpers/http-response')
 
 class SigninRoute {
   constructor (authUseCase) {
@@ -8,24 +9,10 @@ class SigninRoute {
 
   async route (httpRequest) {
     try {
-      if (!httpRequest.body) {
-        return {
-          statusCode: 400,
-          body: new MissingParamError('body')
-        }
-      }
-      if (!httpRequest.body.email) {
-        return {
-          statusCode: 400,
-          body: new MissingParamError('email')
-        }
-      }
-      if (!httpRequest.body.password) {
-        return {
-          statusCode: 400,
-          body: new MissingParamError('password')
-        }
-      }
+      if (!httpRequest.body) return HttpResponse.badRequest(new MissingParamError('body'))
+      if (!httpRequest.body.email) return HttpResponse.badRequest(new MissingParamError('email'))
+      if (!httpRequest.body.password) return HttpResponse.badRequest(new MissingParamError('password'))
+
       const { email, password } = httpRequest.body
 
       const token = await this.authUseCase.verifyLogin(email, password)
