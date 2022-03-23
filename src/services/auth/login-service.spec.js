6fe19c82e4ec3ba75f2eace61fe2e1451b7addc2
luissky <luizguilhermeabrantes@gmail.com>
2022-03-22 23:27:59 -0300

    ensure LoginService throws if no EncrypterHelper is provided

--- src/services/auth/login-service.spec.js
+++ src/services/auth/login-service.spec.js
@@ -1,14 +1,44 @@
 const { ServerError } = require('../../utils/errors')
 const LoginService = require('./login-service')
 
+const makeUserRepoSpy = () => {
+  class UserRepositorySpy {
+    async findOne () {
+    }
+  }
+  const userRepoSpy = new UserRepositorySpy()
+  return userRepoSpy
+}
+
+const makeEncrypterHelperSpy = () => {
+  class EncrypterHelperSpy {
+
+  }
+  const encrypterHelperSpy = new EncrypterHelperSpy()
+  return encrypterHelperSpy
+}
+
 const makeSut = () => {
-  const loginService = new LoginService()
-  return loginService
+  const userRepoSpy = makeUserRepoSpy()
+  const encrypterHelperSpy = makeEncrypterHelperSpy()
+  const sut = new LoginService({ userRepository: userRepoSpy, encryptHelper: encrypterHelperSpy })
+  return {
+    sut,
+    userRepoSpy,
+    encrypterHelperSpy
+  }
 }
 
 describe('LoginService', () => {
   test('Should throw if no UserRepository is provided', async () => {
-    const sut = makeSut()
+    const sut = new LoginService()
+    const promise = sut.verifyLogin()
+    await expect(promise).rejects.toEqual(new ServerError())
+  })
+
+  test('Should throw if no EncrypterHelper is provided', async () => {
+    const { userRepoSpy } = makeSut()
+    const sut = new LoginService({ userRepository: userRepoSpy })
     const promise = sut.verifyLogin()
     await expect(promise).rejects.toEqual(new ServerError())
   })
