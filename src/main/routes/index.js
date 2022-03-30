fb8111010d49c748911702b86549ec71411a57c3
luissky <luizguilhermeabrantes@gmail.com>
2022-03-30 15:55:36 -0300

    refactor: move ExpressAdapter to own file

--- src/main/routes/index.js
+++ src/main/routes/index.js
@@ -1,21 +1,10 @@
 const express = require('express')
 const routes = express.Router()
+const ExpressAdapter = require('./adapters/express-adapter')
 
 // Routes
 const SigninRouter = require('./signin.js')
 const SignupRouter = require('./signup.js')
-
-class ExpressAdapter {
-  static adapt (route) {
-    return async (req, res) => {
-      const httpRequest = {
-        body: req.body
-      }
-      const httpResponse = await route.route(httpRequest)
-      return res.status(httpResponse.statusCode).json(httpResponse.body)
-    }
-  }
-}
 
 routes.post('/signup', ExpressAdapter.adapt(new SignupRouter()))
 routes.post('/signin', ExpressAdapter.adapt(new SigninRouter()))
