27483698390c438420d3c480226c84a45a1d6c10
luissky <luizguilhermeabrantes@gmail.com>
2023-11-26 12:31:28 -0300

    reinstall jest ts-jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^bcrypt$': '<rootDir>/__mocks__/bcrypt.ts',
+}
