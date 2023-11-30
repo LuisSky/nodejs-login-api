db1062df41f3a9f78eb1ef60a81321ab09a2ff0d
luissky <luizguilhermeabrantes@gmail.com>
2023-11-30 18:37:00 -0300

    chore: add .dist to modulePathIgnore

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   testEnvironment: 'node',
   moduleNameMapper: {
     '^bcrypt$': '<rootDir>/__mocks__/bcrypt.ts',
-  }
+  },
+  modulePathIgnorePatterns: ["<rootDir>/dist/"]  
 }  
