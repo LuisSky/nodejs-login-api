cc1dfb24479b30a255d85140f2ca6ba72e1bed52
luissky <luizguilhermeabrantes@gmail.com>
2023-12-09 12:36:24 -0300

    chore: add type to jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+
+const typeTest = process.env.JEST_TYPE_TEST == 'unity' ? '<rootDir>/**/*.spec.ts' : '<rootDir>/**/*.test.ts'
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
     '^bcrypt$': '<rootDir>/__mocks__/bcrypt.ts',
   },
-  modulePathIgnorePatterns: ["<rootDir>/dist/"]  
+  modulePathIgnorePatterns: [
+    "<rootDir>/dist/",
+    "./node_modules"
+  ],
+  testMatch: [typeTest]
 }  
