4ba6260c2f9d72a862f714e57dba682578fe41ef
luissky <luizguilhermeabrantes@gmail.com>
2023-12-10 11:15:23 -0300

    feat: ensure ExpressAdapter receive a Controller protocol

--- src/main/adapters/express-adapter.ts
+++ src/main/adapters/express-adapter.ts
@@ -1,15 +1,16 @@
 
 import { Response } from "express"
 import { httpRequest, httpResponse } from "../../domain/services/auth/interfaces"
+import { Controller } from "../../utils/protocols"
 
 
 export default class ExpressAdapter {
-  static adapt (route: any) {
+  static adapt (controller: Controller) {
     return async (req: httpRequest, res: Response) => {
       const httpRequest = {
         body: req.body
       }
-      const httpResponse: httpResponse = await route.handle(httpRequest)
+      const httpResponse: httpResponse = await controller.handle(httpRequest)
       return res.status(httpResponse.statusCode).json(httpResponse.body)
     }
   }
