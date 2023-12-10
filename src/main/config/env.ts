9f54d54ea5d98dd0e8c519fa9668bbd37786e429
luissky <luizguilhermeabrantes@gmail.com>
2023-12-10 11:25:59 -0300

    chore: add a default port

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,9 +2,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export = {
-  PORT: process.env.SERVER_PORT,
+  PORT: process.env.SERVER_PORT || 3000,
 
-  SECRET_TOKEN_PHRASE: process.env.SECRET_TOKEN_PHRASE || "any_phrase",
+  SECRET_TOKEN_PHRASE: process.env.SECRET_TOKEN_PHRASE || "any_secret_token",
 
   DB_CLIENT: process.env.DB_CLIENT,
   DB_HOST: process.env.DB_HOST,
