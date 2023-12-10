36c1e3abfdb7248cf998219eba6cc36d80de9569
luissky <luizguilhermeabrantes@gmail.com>
2023-12-10 12:51:29 -0300

    refactor: remove https to interfaces.ts

--- src/domain/services/auth/interfaces.ts
+++ src/domain/services/auth/interfaces.ts
@@ -9,14 +9,3 @@ export type IUserRepository = {
   createOne: (user: User) => any,
   findByEmail: (email: string) => any
 }
-
-
-
-export type httpRequest = {
-  body: any
-}
-
-export type httpResponse = {
-  body: any,
-  statusCode: number
-}
