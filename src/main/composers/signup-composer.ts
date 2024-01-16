23fe80a1cf14cc90a9e960d368bc777292b87b19
luissky <luizguilhermeabrantes@gmail.com>
2024-01-15 23:35:27 -0300

    refactor: change SignupComposer to receive a DbAddUserAccount instead a old UserService

--- src/main/composers/signup-composer.ts
+++ src/main/composers/signup-composer.ts
@@ -1,15 +1,16 @@
 import RegisterUserService from '../../domain/services/auth/register-user'
-import { RegExpEmailValidator, EncrypterHelper } from '../../utils/helpers'
 import UserRepository from '../../infra/repository/user-repository'
-import { SignupController } from '../../presentation/controllers/auth'
 import { Controller } from '../../utils/protocols'
+import { DbAddUserAccount } from '../../data/auth/db-add-user-account'
+import { SignupController } from '../../presentation/controllers/auth'
+import { RegExpEmailValidator, EncrypterHelper } from '../../utils/helpers'
 
 export class SignupRouterComposer {
   static compose (): Controller {
     const encrypter = new EncrypterHelper()
     const userRepository = new UserRepository()
     const emailValidator = new RegExpEmailValidator()
-    const registerUserService = new RegisterUserService(userRepository, userRepository, encrypter)
-    return new SignupController(registerUserService, emailValidator)
+    const dbAddUserAccount = new DbAddUserAccount(userRepository, userRepository, encrypter)
+    return new SignupController(dbAddUserAccount, emailValidator)
   }
 }
